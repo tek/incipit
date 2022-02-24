da062bb8d7c2f55dc03574b54febdc13fe156af0
Torsten Schmits <git@tryp.io>
2022-02-24 02:36:15 +0100

    add deps to incipit

--- flake.nix
+++ flake.nix
@@ -8,10 +8,12 @@
 
     all = { hackage, override, ... }: {
       polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
-      polysemy-conc = hackage "0.5.1.1" "1gqyskqkdavbzpqvlhxf3f5j130w06wc7cw8kxn2cayavzd9zl9b";
-      polysemy-resume = hackage "0.2.0.0" "0kh7cwqkr5w69zkm68l6q4d8nkai7fc29n48p3f8skqw638x4w9p";
-      polysemy-test = hackage "0.3.1.9" "1927j1bg2nihq3idcckr1jga8n183s6bh5hdd8hm6qncwksymylg";
-      polysemy-time = hackage "0.2.0.3" "0k78l6h0cf4aj1jaxqzs1v1b9j898asz80gs603nl5nx5ji0g3ng";
+      polysemy-conc = hackage "0.6.0.0" "16b20nlij227pmd2qxq5ad9fr6496y0ammmw2y95x66dz85c5yg4";
+      polysemy-plugin = hackage "0.4.1.0" "117g92l1ppsqd3w0rqjrxfk0lx6yndd54rpymgxljilnv43zg29s";
+      polysemy-log = hackage "0.5.0.0" "0qzgi6mi4is059lb815slzkc95xd2f9ndpfhrb7nh7czlm40pzcn";
+      polysemy-resume = hackage "0.3.0.0" "0kv8x41cxrdwxh7xw8vrywl7sgjkigl84xl7gv038gijh7pvd358";
+      polysemy-test = hackage "0.4.0.1" "038n31xxid72vrckr3afgkvbsvqhf9q4b912agg24ppjzckq2s15";
+      polysemy-time = hackage "0.3.0.0" "0mgiq70b35q7ymfwvb8fv291l3f8v7k0z7w6909h922d6jgl4jgp";
     };
 
     ghc921 = { hackage, ... }: {
@@ -24,13 +26,11 @@
     base = ./.;
     overrides = { inherit all ghc921; };
     packages = {
-      # incipit = ./packages/incipit;
+      incipit = ./packages/incipit;
       incipit-base = ./packages/incipit-base;
       incipit-core = ./packages/incipit-core;
     };
-    main = "incipit-core";
-    hackage = {
-      versionFile = "ops/hpack/shared/meta.yaml";
-    };
+    main = "incipit";
+    hackage.versionFile = "ops/hpack/shared/meta.yaml";
   };
 }
