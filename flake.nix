d0e86b3be9cd1a30850ca16dd09dc3d2b1fe76e7
Torsten Schmits <git@tryp.io>
2022-10-10 01:34:25 +0200

    bump polysemy-log

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     hix.url = git+https://git.tryp.io/tek/hix;
-    polysemy-log.url = "git+https://git.tryp.io/tek/polysemy-log?tag=v0.7.0.0";
+    polysemy-log.url = "git+https://git.tryp.io/tek/polysemy-log?tag=v0.7.2.0";
     polysemy-conc.url = "git+https://git.tryp.io/tek/polysemy-conc";
   };
 
@@ -12,7 +12,7 @@
     all = { hackage, source, ... }: {
       polysemy-chronos = hackage "0.5.1.0" "14rwrhad4fvmxdxyk4qyk05fjlxg0b9bsqcfci1yb58v4fhi432h";
       polysemy-conc = source.package polysemy-conc "conc";
-      polysemy-log = hackage "0.7.1.0" "19f4hcmbf6klgyn48jw4nl54d9xc5x255w259cdpgpn4xkkcxmzw";
+      polysemy-log = hackage "0.7.2.0" "1dpgcvrjgpdpynnx804sdj4kpf06lvq0bqxqc79lazc2q82vaj6s";
       polysemy-plugin = hackage "0.4.1.0" "117g92l1ppsqd3w0rqjrxfk0lx6yndd54rpymgxljilnv43zg29s";
       polysemy-process = source.package polysemy-conc "process";
     };
